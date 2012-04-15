b5bdbfd594d3fac2d87866474b6e8580e0c754fb
Michael Smietana <michael@smietana.se>
2012-04-15 17:06:33 +0200

    Removed hard coded settings

--- BkrAwsConfig.hs
+++ BkrAwsConfig.hs
@@ -19,21 +19,29 @@ import           Aws.Sqs.Info
 
 import qualified Data.Text as T
 
-getS3Config :: Configuration
-getS3Config = do
-  let cr = Credentials (B.pack "AKIAJNREVFV4JQNNNQRQ") (B.pack "dqQmkG7q4TwJemnEoqVeSppTuOcMp6RoBRZCpibp")
-  Configuration { timeInfo = Timestamp
-                , credentials = cr
-                , sdbInfo = sdbHttpsPost sdbUsEast
-                , sdbInfoUri = sdbHttpsGet sdbUsEast
-                , s3Info = s3 HTTP s3EndpointUsClassic False
-                , s3InfoUri = s3 HTTP s3EndpointUsClassic True
-                , sqsInfo = sqs HTTP sqsEndpointUsClassic False
-                , sqsInfoUri = sqs HTTP sqsEndpointUsClassic True
-                , sesInfo = sesHttpsPost sesUsEast
-                , sesInfoUri = sesHttpsGet sesUsEast
-                , logger = defaultLog Warning
-                }
+import BkrConfig
+import Data.Maybe (fromJust)
 
-getS3BucketName :: T.Text
-getS3BucketName = T.pack "ms-bkr"
+getS3Config :: IO Configuration
+getS3Config = do
+     -- Get AWS access and secret keys from bkr.conf
+     confPairs <- getConfPairsFromFileS' "bkr.conf"
+     let cr = Credentials (B.pack $ fromJust $ lookup "awsaccesskeyid" confPairs) (B.pack $ fromJust $ lookup "awssecretaccesskey" confPairs)
+     
+     return Configuration { timeInfo = Timestamp
+                          , credentials = cr
+                          , sdbInfo = sdbHttpsPost sdbUsEast
+                          , sdbInfoUri = sdbHttpsGet sdbUsEast
+                          , s3Info = s3 HTTP s3EndpointUsClassic False
+                          , s3InfoUri = s3 HTTP s3EndpointUsClassic True
+                          , sqsInfo = sqs HTTP sqsEndpointUsClassic False
+                          , sqsInfoUri = sqs HTTP sqsEndpointUsClassic True
+                          , sesInfo = sesHttpsPost sesUsEast
+                          , sesInfoUri = sesHttpsGet sesUsEast
+                          , logger = defaultLog Warning
+                          }
+
+getS3BucketName :: IO T.Text
+getS3BucketName = do
+     confPairs <- getConfPairsFromFileS' "bkr.conf"
+     return $ T.pack $ fromJust $ lookup "s3bucketname" confPairs
