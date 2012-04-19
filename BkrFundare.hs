374d0415ab8c7c27d57da308c83940a167d5bfc3
Michael Smietana <michael@smietana.se>
2012-04-19 10:23:06 +0200

    BkrMeta path, fileChec, pathChec, Eq
    - BkrMeta with fullPath, fileChecksum and pathChecksum
    - Added an Eq instance that compares the file and path checksums

--- BkrFundare.hs
+++ BkrFundare.hs
@@ -3,9 +3,19 @@ module BkrFundare ( BkrMeta(..)
                     --,
                   ) where
 
+{-
 data BkrMeta = BkrMeta          { fullPath :: FilePath
                                 , checksum :: String
                                 }         
              | BkrMetaCheck     { pathChecksum :: String
                                 , fileChecksum :: String
                                 } deriving (Show, Eq)
+-}
+
+data BkrMeta = BkrMeta { fullPath :: FilePath
+                       , fileChecksum :: String
+                       , pathChecksum :: String
+                       } deriving (Show)
+
+instance Eq BkrMeta where
+     meta1 == meta2 = fileChecksum meta1 == fileChecksum meta2 && pathChecksum meta1 == pathChecksum meta2
