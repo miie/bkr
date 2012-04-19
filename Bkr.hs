5531d2b4971fb497ef28997b90dfc7c3e47a15cb
Michael Smietana <michael@smietana.se>
2012-04-19 21:50:54 +0200

    Clean up

--- Bkr.hs
+++ Bkr.hs
@@ -22,30 +22,25 @@ main = do
      -- Get BkrMeta objects from S3
      logNotice "Getting Bkr files from S3"
      bkrS3Meta <- S3B.getBkrObjects
-     --debugF $ show bkrS3Meta
+
      -- Get local BkrMeta objects
      logNotice "Getting local files"
-     --confPairs <- getConfPairsFromFileS' "bkr.conf"
-     --bkrLocalMeta <- mapM F.getBkrObjects (split "," (fromJust $ getValueS "folderstobackup" confPairs)) -- -> move the getting folders to backup to BkrConfig + remove spaces before and after and normalize
      backupFolders <- getBackupFolders
      bkrLocalMeta <- mapM F.getBkrObjects backupFolders
      
-     -- Filter the objects and get local objects that are not on S3
+     -- Filter objects to get local objects that are not backed up
      logNotice "Checking which files should be uploaded"
      let objToUpload = filter (`notElem` bkrS3Meta) (concat bkrLocalMeta)
-     --print $ show objToUpload
      -- Create a list with a triple (bkrObj, no of bkrObj, nth bkrObj) to use as a counter
      let len = length objToUpload
      let counterList = zip3 objToUpload [len | x <- [1..]] [1..] -- We can use non ending lists since zip ends when the shortest (objToUpload) list ends. Got to love this lazy stuff.
      
      logNotice $ (show len) ++ " files will be uploaded"
      -- For each element in objToUpload upload the local file then create a .bkrm file and upload it
-     --mapM putFiles objToUpload
      mapM putFiles counterList
      logNotice "done"  
 
 putFiles :: (BkrMeta, Int, Int) -> IO ()
---putFiles bkrObj = do
 putFiles (bkrObj, len, nthObj) = do
      
      let localPath = fullPath bkrObj
