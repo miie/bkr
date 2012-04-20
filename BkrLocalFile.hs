760105aa236bd00fda8ffdcb2d9818df1f4731aa
Michael Smietana <michael@smietana.se>
2012-04-20 22:30:11 +0200

    Added filtering on founders, files and file extensions

--- BkrLocalFile.hs
+++ BkrLocalFile.hs
@@ -8,7 +8,7 @@ module BkrLocalFile ( getBkrObjects
 
 import Control.Monad (forM, mapM)
 import System.Directory (doesDirectoryExist, getDirectoryContents, doesFileExist)
-import System.FilePath ((</>), normalise)
+import System.FilePath ((</>), normalise, takeExtensions, takeDirectory)
 import Prelude hiding (catch)
 import Control.Exception
 
@@ -17,18 +17,48 @@ import qualified Data.ByteString.Lazy as L
 
 import Hasher
 import BkrFundare
+import BkrConfig (getFilesToIgnore, getFileExtensionsToIgnore, getFoldersToIgnore)
+
+filterExt :: [FilePath] -> [FilePath] -> [FilePath]
+filterExt ignoreList = filter ((\x -> (takeExtensions x) `notElem` ignoreList))
+
+filterFolder :: [FilePath] -> [FilePath] -> [FilePath]
+filterFolder ignoreList = filter ((\x -> (takeDirectory x) `notElem` ignoreList))
+
+filterFile :: [FilePath] -> [FilePath] -> [FilePath]
+filterFile ignoreList = filter (\x -> x `notElem` ignoreList)
 
 getAllFiles :: FilePath -> IO [FilePath]
 getAllFiles topPath = do
   names <- getDirectoryContents topPath `catch` \ (ex :: IOException) -> handleIO ex topPath
-  let properNames = filter (`notElem` [".", "..", ".DS_Store", ".localized"]) names
+  filesToIgnore <- getFilesToIgnore
+  fileExtToIgnore <- getFileExtensionsToIgnore
+  foldersToIgnore <- getFoldersToIgnore
+  -- Filter files on extension and files to ignore
+  --let properNames = (filterExt fileExtToIgnore) $ filter (`notElem` ([".", ".."] ++ filesToIgnore)) names
+  let properNames = (filterExt fileExtToIgnore) $ filterFile ([".", ".."] ++ filesToIgnore) names
+  -- Map files with topPath to get full path and filter on folders to ignore
+  let allPaths = (filterFolder foldersToIgnore) $ map (topPath </>) properNames
+  paths <- forM allPaths $ \path -> do
+    isDirectory <- doesDirectoryExist path
+    if isDirectory
+      then getAllFiles path
+      else return [normalise path]
+  return $ concat paths
+
+getAllFilesOld :: FilePath -> IO [FilePath]
+getAllFilesOld topPath = do
+  names <- getDirectoryContents topPath `catch` \ (ex :: IOException) -> handleIO ex topPath
+  filesToIgnore <- getFilesToIgnore
+  let properNames = filter (`notElem` ([".", ".."] ++ filesToIgnore)) names
+  print $ show properNames
   paths <- forM properNames $ \name -> do
     let path = topPath </> name
     isDirectory <- doesDirectoryExist path
     if isDirectory
       then getAllFiles path
       else return [normalise path]
-  return (concat paths)
+  return $ concat paths
 
 handleIO :: IOException -> FilePath -> IO [FilePath]
 handleIO ex path = do
