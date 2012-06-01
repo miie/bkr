ccd99b54d8e90891013b0a17c5d1d224a1c1e898
Michael Smietana <michael@smietana.se>
2012-06-01 23:04:09 +0200

    BkrLogging - handle wrongly configured log file gracefully

--- src/Bkr/BkrLogging.hs
+++ src/Bkr/BkrLogging.hs
@@ -22,25 +22,26 @@ setupLogging :: Priority -> IO ()
 setupLogging logLevel = do
      -- Get log file location
      logFilePath <- getLogFileLocation
+     --print $ show logFilePath
+     -- Check if we got a log file, if not set up logging to stderr otherwise set up file logging
+     if logFilePath == Nothing
+        then do
+              putStrLn "Could not find log file, please make sure to set the log file path correctly in bkr settings. Will log to stderr."
+              updateGlobalLogger "bkrfile" (setLevel logLevel)
+        else do
+              -- Check that the log file hasn't reached maximum size and recycle if it has
+              logFileMaxSize <- getLogFileMaximumSize
+              fileStatus <- getFileStatus (fromJust logFilePath)
+              let logFileSize = fileSize fileStatus
+              when ((read (show logFileSize) :: Int) > logFileMaxSize) (removeFile $ fromJust logFilePath)
 
-     -- Check that the log file hasn't reached maximum size and recycle if it has
-     logFileMaxSize <- getLogFileMaximumSize
-     fileStatus <- getFileStatus (fromJust logFilePath)
-     let logFileSize = fileSize fileStatus
-     when ((read (show logFileSize) :: Int) > logFileMaxSize) (removeFile $ fromJust logFilePath)
-
-     -- File handler
-     h <- fileHandler (fromJust logFilePath) DEBUG >>= \lh -> return $ setFormatter lh (tfLogFormatter "%F %X.%q" "$time|$loggername|$prio: $msg")
-     
-     -- Growl handler
-     --g <- growlHandler "bkrgrow" NOTICE
-     --addTarget "localhost" g
-     
-     updateGlobalLogger "bkrfile" (addHandler h)
-     --updateGlobalLogger "bkrgrowl" (addHandler g)
-     
-     updateGlobalLogger "bkrfile" (setLevel logLevel)
-     --updateGlobalLogger "bkrgrowl" (setLevel NOTICE)
+              -- File handler
+              h <- fileHandler (fromJust logFilePath) DEBUG >>= \lh -> return $ setFormatter lh (tfLogFormatter "%F %X.%q" "$time|$loggername|$prio: $msg")
+        
+              -- Update global logger with handler and log level
+              updateGlobalLogger "bkrfile" (addHandler h)
+              updateGlobalLogger "bkrfile" (setLevel logLevel)
+        
 
 logDebug :: String -> IO ()
 logDebug = debugM "bkrfile"
