b033e1207f68dea739a7e15bc96e405939f5aaea
Michael Smietana <michael@smietana.se>
2012-06-01 23:04:26 +0200

    Hasher - clean up

--- src/Bkr/Hasher.hs
+++ src/Bkr/Hasher.hs
@@ -6,21 +6,22 @@ module Bkr.Hasher ( getFileHash
 
 import Data.Digest.Pure.MD5 (md5, MD5Digest)
 
-import qualified Data.ByteString.Lazy.UTF8 as UB
-import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.UTF8 as UBL
+import qualified Data.ByteString.Lazy as BL
 
-{-| Gets a MD5Digest for a file. |-}
+{-| Gets a MD5Digest for a file. -}
 getFileHash :: FilePath -> IO MD5Digest
 getFileHash path = do
      --L.readFile path >>= return . md5
      
-     readF <- B.readFile path
+     readF <- BL.readFile path
      return $! md5 readF
 
 {-| Gets a MD5Digest for a String. Example:
+
 @
-print $ show $ getHashForString "sdknafsfadsäöåfas"
+print $ show $ getHashForString \"sdknafsfadsäöåfas\"
 @
-|-}
+-}
 getHashForString :: String -> MD5Digest
-getHashForString = md5 . UB.fromString
+getHashForString = md5 . UBL.fromString
