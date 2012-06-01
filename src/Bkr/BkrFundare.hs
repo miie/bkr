1c294bc9eda8c5b5ccc4caaa0601b1439e25b2c0
Michael Smietana <michael@smietana.se>
2012-06-01 23:02:37 +0200

    BkrFundare - documentation clean up

--- src/Bkr/BkrFundare.hs
+++ src/Bkr/BkrFundare.hs
@@ -19,10 +19,11 @@ True for:
 fileChecksum && pathChecksum
 or
 pathChecksum && modificationTimeChecksum
-|-}
+-}
 instance Eq BkrMeta where
      meta1 == meta2 = (fileChecksum meta1 == fileChecksum meta2 && pathChecksum meta1 == pathChecksum meta2) || (pathChecksum meta1 == pathChecksum meta2 && modificationTimeChecksum meta1 == modificationTimeChecksum meta2)
 
+{-| Gets BkrMeta for a FilePath. -}
 getBkrMeta :: FilePath -> IO BkrMeta
 getBkrMeta path = do
      fileHash <- getFileHash path
