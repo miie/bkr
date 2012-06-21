b810a87fd34f64eb7fc126adef8cd4854b9017d1
Michael Smietana <michael@smietana.se>
2012-06-21 13:14:56 +0200

    BkrS3Bucket - added:
    - deleteBackedFile
    - saveObject
    - getBkrObject
    - getFileNameForPut

--- src/System/Bkr/TargetServices/S3/BkrS3Bucket.hs
+++ src/System/Bkr/TargetServices/S3/BkrS3Bucket.hs
@@ -1,30 +1,35 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
 module System.Bkr.TargetServices.S3.BkrS3Bucket ( getBkrObjects
-                                         , putBackupFile
-                                         , putBkrMetaFile
-                                         , deleteS3Object
-                                         ) where
+                                                , putBackupFile
+                                                , putBkrMetaFile
+                                                , deleteBackedFile
+                                                --, getFileNameForPut
+                                                ) where
 
-import System.Bkr.BkrConfig
-import System.Bkr.BkrFundare
-import System.Bkr.Hasher
-import System.Bkr.BkrLogging
-import System.Bkr.TargetServices.S3.BkrAwsConfig
+import System.Bkr.BkrConfig (getUseS3ReducedRedundancy, getConfPairsFromByteString')
+import System.Bkr.BkrFundare (BkrMeta(..))
+import System.Bkr.Hasher (getHashForString)
+import System.Bkr.BkrLogging (logCritical, logDebug, logNotice)
+import System.Bkr.TargetServices.S3.BkrAwsConfig (getS3Config, getS3BucketName)
 
-import System.IO
-import Network.HTTP.Conduit
+import System.IO (IOMode(..), Handle, hClose)
+import Network.HTTP.Conduit (RequestBody(RequestBodyBS))
 import Data.IORef (newIORef, readIORef)
 import Data.Monoid (mempty)
 import System.FilePath.Posix (takeFileName)
 import Prelude hiding (catch)
 import Control.Concurrent (threadDelay)
+import Data.Maybe (fromJust)
+import Data.Conduit (($$))
+import Data.Conduit.Binary (sinkIOHandle)
 
 import qualified Aws
 import qualified Aws.S3 as S3
 import qualified Data.Text as T
 import qualified Data.ByteString as B
 import qualified Control.Exception as C
+import qualified Data.Knob as K
 
 getBkrObjectKeys :: T.Text -> [T.Text] -> IO [T.Text]
 getBkrObjectKeys gbMarker objList = do
@@ -81,19 +86,27 @@ getMetaKeys key = BkrMeta { fullPath                 = ""
                           }
                           where kSplit = T.split (=='.') key
 
+{-| TODO: add more doc...
+
+Replace space with underscore in the upload name (S3 does not handle blanks in object names). Doing this is safe since the whole original path is stored in the meta file. -}
+getFileNameForPut :: FilePath -> T.Text
+getFileNameForPut filePath = T.replace " " "_" $ T.pack $ show (getHashForString filePath) ++ "::" ++ takeFileName filePath
+
 putBackupFile :: FilePath -> IO ()
 putBackupFile filePath = do
         
-     let uploadName = T.pack $ show (getHashForString filePath) ++ "::" ++ takeFileName filePath
+     --let uploadName = T.pack $ show (getHashForString filePath) ++ "::" ++ takeFileName filePath
+     
      -- Get MD5 hash for file
      --contentMD5 <- getFileHash path
      --putFile path uploadName (Just $ BUTF8.fromString $ show contentMD5)
-     putFile filePath uploadName Nothing 0
+     --putFile filePath uploadName Nothing 0
+     putFile filePath (getFileNameForPut filePath) Nothing 0
 
 putBkrMetaFile :: FilePath -> IO ()
 putBkrMetaFile filePath = do
 
-     let uploadName = T.pack $ "bkrm." ++ takeFileName filePath
+     let uploadName = T.pack $ takeFileName filePath
      -- Get MD5 hash for file
      --contentMD5 <- getFileHash path
      --putFile path uploadName (Just $ BUTF8.fromString $ show contentMD5)
@@ -158,6 +171,16 @@ putFile' filePath uploadName contentMD5 = do
      --logDebug $ "putFile: response metadata: " ++ (show ioResponseMetaData)
      readIORef metadataRef >>= logDebug . ("putFile: response metadata: " ++) . show
 
+deleteBackedFile :: T.Text -> IO ()
+deleteBackedFile bkrmFileName = do
+
+     -- Get BkrMeta from the backed upped bkrm file
+     bkrMeta <- getBkrObject bkrmFileName
+
+     -- Delete the backed file and the bkrm file
+     _ <- mapM deleteS3Object [(getFileNameForPut $ fullPath bkrMeta), bkrmFileName]
+     return ()
+
 deleteS3Object :: T.Text -> IO ()
 deleteS3Object objectName = do
      
@@ -178,3 +201,39 @@ deleteS3Object objectName = do
 
      logDebug "deleteObject: delete done"
      readIORef metadataRef >>= logDebug . ("deleteObject: response metadata: " ++) . show
+
+{-| A small function to save the object's data into a file handle. -}
+saveObject :: IO Handle -> Aws.HTTPResponseConsumer ()
+saveObject hndl _ _ source = source $$ sinkIOHandle hndl
+
+{-| Takes the name of a backed upped bkrm file and returns the corresponding BkrMeta object. Example of a bkrm file: bkrm.ae15567db73c2bc31a8c4140098ef49b.19b6056f332ecc693ffdee66dbe33896.
+
+This function uses the Knob package for in-memory temporary storage of the downloaded bkrm file.
+-}
+getBkrObject :: T.Text -> IO BkrMeta
+getBkrObject bkrmFileName = do
+     
+     -- Get S3 config
+     cfg <- getS3Config
+
+     -- Create an IORef to store the response Metadata (so it is also available in case of an error).
+     metadataRef <- newIORef mempty 
+     
+     -- Get knob object and knob handle (knob is a in-memory virtual file) 
+     knob <- K.newKnob (B.pack [])
+     knobHndl <- K.newFileHandle knob "tmp.knob" WriteMode
+     
+     -- Get the .bkrm file
+     bucketName <- getS3BucketName
+     _ <- Aws.simpleAwsRef cfg metadataRef $ S3.getObject bucketName bkrmFileName (saveObject $ return knobHndl)
+     -- Get data (text) from the knob virtual file
+     knobDataContents <- K.getContents knob
+     -- Close knob
+     hClose knobHndl
+     -- Get the config pair and get path and checksum from the pair
+     pairS <- getConfPairsFromByteString' knobDataContents
+     let path_                     = fromJust $ lookup "fullpath" pairS
+     let checksum_                 = fromJust $ lookup "checksum" pairS
+     let modificationTime_         = fromJust $ lookup "modificationtime" pairS
+     let modificationTimeChecksum_ = fromJust $ lookup "modificationtimechecksum" pairS
+     return $ BkrMeta path_ checksum_ (show $ getHashForString path_) modificationTime_ modificationTimeChecksum_
