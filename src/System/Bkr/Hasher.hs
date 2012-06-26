cddcde8643a065c6e9aeda322b842f57eecd0f4f
Michael Smietana <michael@smietana.se>
2012-06-26 13:41:01 +0200

    Hasher - added getFileHash':
    Gets MD5 hash for file using the crypto-hash package

--- src/System/Bkr/Hasher.hs
+++ src/System/Bkr/Hasher.hs
@@ -1,11 +1,14 @@
 
 
 module System.Bkr.Hasher ( getFileHash
+                         , getFileHash'
                          , getHashForString
                          ) where
 
 import Data.Digest.Pure.MD5 (md5, MD5Digest)
+import Crypto.Hash.MD5 (hash)
 
+import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy.UTF8 as UBL
 import qualified Data.ByteString.Lazy as BL
 
@@ -16,6 +19,12 @@ getFileHash path = do
      
      readF <- BL.readFile path
      return $! md5 readF
+
+{-| Gets a MD5 hash for file using the crypto-hash package -}
+getFileHash' :: FilePath -> IO B.ByteString
+getFileHash' path = do
+
+     B.readFile path >>= return . hash
 
 {-| Gets a MD5Digest for a String. Example:
 
